9f6faf4845304fa177afadcbf50446c345829e76
kripso <kripsoworld@gmail.com>
2021-03-13 14:55:27 +0100

    amd gpu script

--- WebScraping/amd_GPU_allert.py
+++ WebScraping/amd_GPU_allert.py
@@ -1,29 +1,95 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pandas as pd
+import platform
+import smtplib
 
-
+# user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+browser = None
 options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--allow-running-insecure-content')
+
 options.add_argument('--headless')
-options.add_argument("--window-size=1920,1080")
+options.add_argument('--no-sandbox')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
+options.add_argument("--window-size=1920,1080")
 options.add_argument('--ignore-certificate-errors')
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-options.add_argument(f'user-agent={user_agent}')
-driver = webdriver.Chrome(executable_path='D:\Krips\Documents\Programing\python_exp\WebScraping\chromedriver.exe', chrome_options=options)
+options.add_argument('--allow-running-insecure-content')
+options.add_argument(f'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
 
-# driver = webdriver.Chrome('D:\Krips\Documents\Programing\python_exp\WebScraping\chromedriver.exe')
+if (platform.system() == 'Windows'):
+    browser = webdriver.Chrome(executable_path='D:\Krips\Documents\Programing\python_exp\WebScraping\chromedriver.exe', chrome_options=options)
+else:
+    browser = webdriver.Chrome('D:\Krips\Documents\Programing\python_exp\WebScraping\chromedriver', chrome_options=options)
 
-driver.get('https://www.amd.com/en/direct-buy/sk')
-products = driver.find_elements_by_xpath('//div[@class="direct-buy"]')
 
-rx6800xt = []
-for div in range(len(products)):
-    if 'AMD Radeon™ RX 6800 XT Graphics' in products[div].text:
-        rx6800xt.append(products[div].text)
+def sendSmtpEmail(to, message):
+    # creates SMTP session
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
 
-print(rx6800xt)
+    # start TLS for security
+    smtp.starttls()
+
+    # Authentication
+    smtp.login("scripnotifier@gmail.com", "kripsi291KRIPSI")
+
+    # sending the mail
+    smtp.sendmail('scripnotifier@gmail.com', to, message)
+
+
+def ryzenInStock(model):
+    message = """Subject: Stock Update!!!
+
+                AMD RYZEN {} Processor Is In Stock!!!
+                GO To: https://www.amd.com/en/direct-buy/sk
+                """.format(model)
+    return (message)
+
+
+def radeonInStock(model):
+    message = """Subject: Stock Update!!!
+
+                AMD Radeon {} Graphics Is In Stock!!!
+                GO To: https://www.amd.com/en/direct-buy/sk
+                """.format(model)
+    return (message)
+
+
+def productParser():
+    browser.get('https://www.amd.com/en/direct-buy/sk')
+    products = browser.find_elements_by_xpath('//div[@class="direct-buy"]')
+
+    for div in range(len(products)):
+        if 'AMD Radeon™ RX 6800 XT Graphics' in products[div].text:
+            RX_6800XT = products[div].text
+        if 'AMD RYZEN™ 9 5900X Processor' in products[div].text:
+            RYZEN_9_5900X = products[div].text
+        # if "AMD RYZEN™ 7 5800X Processor" in products[div].text:
+        #     RYZEN_7_5800X = products[div].text
+
+    # print(RX_6800_XT)
+    # print(RYZEN_9_5900X)
+
+    messages = []
+
+    if "Out of Stock" not in RX_6800XT:
+        messages.append(radeonInStock('RX 6800XT'))
+
+    if "Out of Stock" not in RYZEN_9_5900X:
+        messages.append(ryzenInStock('RYZEN 9 5900X'))
+
+    # if "Out of Stock" not in RYZEN_7_5800X:
+    #     return ryzenInStock('RYZEN 7 5800X')
+
+    if messages == []:
+        return None
+    else:
+        return messages
+
+
+if __name__ == '__main__':
+    messages = productParser()
+
+    if messages:
+        for message in messages:
+            sendSmtpEmail('kripsoworld@gmail.com', message)
+
+    browser.close()
